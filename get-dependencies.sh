31050d8c0ddbcb457d093e8f121c4a74fcd243d0
juju4 <juju4@users.noreply.github.com>
2018-12-08 17:50:04 -0500

    update galaxy naming - get-dependency too

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,8 +12,7 @@ fi
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.aptcacherngclient ] && ln -s ansible-apt-cacher-ng-client $rolesdir/juju4.apt-cacher-ng-client
-[ ! -e $rolesdir/juju4.aptcacherngclient ] && cp -R $rolesdir/ansible-apt-cacher-ng-client $rolesdir/juju4.apt-cacher-ng-client
+[ ! -e $rolesdir/juju4.aptcacherngclient ] && cp -R $rolesdir/ansible-apt-cacher-ng-client $rolesdir/juju4.aptcacherngclient
 
 ## don't stop build on this script return code
 true
-
